d67918e5ef7f9026d7184c672c82511e51a4a86d
chaitanya <chaitanyagunjapalli@gmail.com>
2026-03-19 15:00:40 +0530

    Basic Health check Endpoint

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -3,10 +3,17 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  constructor(private readonly appService: AppService) {}
+
   @Get('test')
   testConnection() {
     return {
       message: 'Backend connected successfully',
     };
+  }
+
+  @Get('health')
+  healthCheck() {
+    return this.appService.getHealth();
   }
 }
